84a6b81a982accec76c6061a8e3e3ce65df2a81b
biccdirk <dirk.belger@dertour.com>
2025-04-28 08:39:38 +0000

    parametrize pipeline

--- steps/01_setup_snowflake.sql
+++ steps/01_setup_snowflake.sql
@@ -7,7 +7,7 @@ CREATE OR ALTER WAREHOUSE COMPUTE_WH
 
 
 -- Separate database for git repository
-CREATE OR ALTER DATABASE QUICKSTART_COMMON;
+CREATE OR ALTER DATABASE QUICKSTART_{{environment}}; 
 
 
 -- API integration is needed for GitHub integration
